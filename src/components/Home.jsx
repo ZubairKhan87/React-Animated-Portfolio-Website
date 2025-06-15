cef4162aa74b85f978940648cd8faa12a142ca5f
bairi56 <zubair413427@gmail.com>
2025-06-15 18:29:50 +0500

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import pdf from "../pdf/Faizan_Resume.pdf";
+import pdf from "../pdf/Zubair_Khan_Resume.pdf";
 import hero from "./data/hero.json";
 import Typed from "typed.js";
 
